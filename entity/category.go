898f86a6c08535f8d2cd5f99972f551daf730de5
Fenrip <99891614+Venrir@users.noreply.github.com>
2025-06-04 18:28:36 +0300

    initial commit

--- entity/category.go
+++ entity/category.go
@@ -1,6 +1,18 @@
 package entity
 
+import (
+	"errors"
+	"strings"
+)
+
 type Category struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name"`
+}
+
+func (c *Category) Validate() error {
+	if len(strings.TrimSpace(c.Name)) < 2 {
+		return errors.New("category name too short")
+	}
+	return nil
 }
